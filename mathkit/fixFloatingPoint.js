e7e4874a52da6c547ec7f23ed1655469223948d2
kdog3682 <kdog3682@gmail.com>
2024-03-15 00:02:06 -0400

    nerdSolver.js is working!

--- mathkit/fixFloatingPoint.js
+++ mathkit/fixFloatingPoint.js
@@ -2,6 +2,9 @@ export {
     fixFloatingPoint,
 }
 function fixFloatingPoint(value, maxPrecision = 1) {
+    if (maxPrecision === 0) {
+        return parseFloat(value.toFixed(3))
+    }
     for (let p = 0; p <= maxPrecision; p++) {
         const rounded = parseFloat(value.toFixed(p))
         if (Math.abs(value - rounded) < Math.pow(10, - (p + 1))) {
